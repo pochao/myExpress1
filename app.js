d91f89f77b9c5fc672886d29dd50fc9f5d0e92ac
Bob Huang <pochao.huang@hotmail.com>
2018-04-16 02:32:15 +0000

    ok

--- app.js
+++ app.js
@@ -33,7 +33,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/users', users);
 app.use('/calendar', calendar);
 var routes_todo = require('./routes/todoListRoutes'); //importing route
 routes_todo(app); //register the route
