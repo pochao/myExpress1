c8e2d60bc3452140596c94f2740748b7e0acca5f
Bob Huang <pochao.huang@hotmail.com>
2018-04-19 03:06:56 +0000

    ok

--- libs/forgeApi.js
+++ libs/forgeApi.js
@@ -2,7 +2,7 @@ var http2 = require('https');
 
 exports.getToken = function () {
   var dataString = "client_id=" + process.env.FORGEID + "&client_secret=" + process.env.FORGEPASSWORD + "&grant_type=client_credentials";
-  console.log(process.env.FORGEID);
+  console.log(dataString);
   var headers = {
     "Content-Type": "application/x-www-form-urlencoded"
   };
@@ -30,7 +30,7 @@ exports.getToken = function () {
     });
 
     res.on("end", function () {
-        console.log(responseString);
+        //console.log(responseString);
         var ores = JSON.parse(responseString);
         
         return ores.access_token;
