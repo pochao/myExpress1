73284e1cff46e0a479f2bb1ca9afab2d69802a54
Bob Huang <pochao.huang@hotmail.com>
2018-04-19 03:17:04 +0000

    ok

--- libs/forgeApi.js
+++ libs/forgeApi.js
@@ -1,40 +1,24 @@
-var http2 = require('https');
 
 exports.getToken = function () {
-  var dataString = "client_id=" + process.env.FORGEID + "&client_secret=" + process.env.FORGEPASSWORD + "&grant_type=client_credentials&scope=data:read";
-  console.log(dataString);
-  var headers = {
-    "Content-Type": "application/x-www-form-urlencoded"
-  };
-
-  var options = {
-    host: "developer.api.autodesk.com",
-    port: 443,
-    path: "/authentication/v1/authenticate",
-    method: "POST",
-    headers: headers,
-
-    // only for dev!
-    rejectUnauthorized: false,
-    requestCert: true,
-    agent: false
-  };
-
-
-  var req = http2.request(options, function (res) {
-    res.setEncoding("utf8");
-    var responseString = "";
-
-    res.on("data", function (data) {
-        responseString += data;
-    });
-
-    res.on("end", function () {
-        console.log(responseString);
-        var ores = JSON.parse(responseString);
-        
-        return ores.access_token;
-    });
+  var request = require("request");
+  
+  var options = { method: 'POST',
+    url: 'https://developer.api.autodesk.com/authentication/v1/authenticate',
+    headers: 
+     { 'Postman-Token': 'e76f5865-5353-4ea2-b9f3-4675610fa14a',
+       'Cache-Control': 'no-cache',
+       'Content-Type': 'application/x-www-form-urlencoded' },
+    form: 
+     { client_id: process.env.FORGEID,
+       client_secret: process.env.FORGEPASSWORD,
+       grant_type: 'client_credentials',
+       scope: 'data:read' } };
+  
+  request(options, function (error, response, body) {
+    if (error) throw new Error(error);
+  
+    console.log(body);
   });
+
 
 }
