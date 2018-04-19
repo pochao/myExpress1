9d32894ee0b897ff46922b432b7469b6198d4f48
Bob Huang <pochao.huang@hotmail.com>
2018-04-19 05:52:26 +0000

    ok

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -33,32 +33,6 @@ module.exports = function(app) {
     });
 
   // Forge 3D Model Routes
-  app.route('/forge2')
-    .get(function(req, res){
-      var request = require("request");
-      
-      var options = { method: 'POST',
-        url: 'https://developer.api.autodesk.com/authentication/v1/authenticate',
-        headers: 
-         { 'Postman-Token': 'e76f5865-5353-4ea2-b9f3-4675610fa14a',
-           'Cache-Control': 'no-cache',
-           'Content-Type': 'application/x-www-form-urlencoded' },
-        form: 
-         { client_id: process.env.FORGEID,
-           client_secret: process.env.FORGEPASSWORD,
-           grant_type: 'client_credentials',
-           scope: 'data:read' } };
-      
-      request(options, function (error, response, body) {
-        if (error) throw new Error(error);
-        
-        var ores = JSON.parse(body);
-        //console.log(ores.access_token)
-        res.render('forge', { token: ores.access_token });
-      });
-
-    });
-
   app.route('/forge')
     .get(function(req, res){
       forge.getToken(function(token){
